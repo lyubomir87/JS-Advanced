48bc0fd4bfb02a2b9f9887dfaa127b49d6890abb
lyubomir87 <52287972+lyubomir87@users.noreply.github.com>
2020-06-22 16:25:56 +0300

    Create constructionCrew.js

---
+++ Object compositions/constructionCrew.js
@@ -0,0 +1,9 @@
+function solve(worker) {
+
+    if (worker.dizziness === true) {
+        worker.levelOfHydrated += worker.weight * worker.experience * 0.1;
+        worker.dizziness = false;
+    }
+
+    return worker;
+}
